ffd25b573db5eee23bfd3ba4c261c4b7e566ad8d
KaiIshino <s2022004@stu.musashino-u.ac.jp>
2022-09-20 08:44:43 +0900

    SearchItemの除外

--- flaskdb/forms.py
+++ flaskdb/forms.py
@@ -88,21 +88,21 @@ class AddReviewForm(FlaskForm):
         item.itemname = self.companyname.data
         item.grade = self.grade.data
 
-class SearchItemForm(FlaskForm):
-    itemname = StringField(
-        "Item Name",
-        validators = [
-            DataRequired(message="Item Name is required."),
-        ],
-    )
-    cancel = ButtonField("Cancel")
-    submit = SubmitField("Submit")
+# class SearchItemForm(FlaskForm):
+#     itemname = StringField(
+#         "Item Name",
+#         validators = [
+#             DataRequired(message="Item Name is required."),
+#         ],
+#     )
+#     cancel = ButtonField("Cancel")
+#     submit = SubmitField("Submit")
 
-    def copy_from(self, item):
-        self.itemname.data = item.itemname
+#     def copy_from(self, item):
+#         self.itemname.data = item.itemname
 
-    def copy_to(self, item):
-        item.itemname = self.itemname.data
+#     def copy_to(self, item):
+#         item.itemname = self.itemname.data
 
 class CheckOutForm(FlaskForm):
     cancel = ButtonField("Cancel")
